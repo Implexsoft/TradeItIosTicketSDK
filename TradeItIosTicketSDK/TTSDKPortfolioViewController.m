31f3140db20fb41f431a52819a8f6497ac5c5f5f
daniel vaughn <danielbvaughn@gmail.com>
2015-12-17 13:02:48 -0500

    Small cleanup for portfolio

--- TradeItIosTicketSDK/TTSDKPortfolioViewController.m
+++ TradeItIosTicketSDK/TTSDKPortfolioViewController.m
@@ -17,23 +17,14 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view.
+
+
 }
 
-- (void)didReceiveMemoryWarning {
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
-}
 
-/*
-#pragma mark - Navigation
 
-// In a storyboard-based application, you will often want to do a little preparation before navigation
-- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
-    // Get the new view controller using [segue destinationViewController].
-    // Pass the selected object to the new view controller.
-}
-*/
+
+
 
 - (NSInteger)numberOfSectionsInTableView:(UITableView *)tableView {
     return 1;
@@ -55,6 +46,13 @@
     
 
     return cell;
+}
+
+
+#pragma mark - Navigation
+
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+
 }
 
 @end
