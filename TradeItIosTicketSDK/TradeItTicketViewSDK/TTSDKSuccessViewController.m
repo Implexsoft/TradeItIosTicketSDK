2eefab649dd01b149191e5361eca81ddf3676862
daniel vaughn <danielbvaughn@gmail.com>
2016-01-26 16:26:23 -0500

    Add button to make another trade

--- TradeItIosTicketSDK/TradeItTicketViewSDK/TTSDKSuccessViewController.m
+++ TradeItIosTicketSDK/TradeItTicketViewSDK/TTSDKSuccessViewController.m
@@ -10,15 +10,29 @@
 #import "TTSDKUtils.h"
 
 @interface TTSDKSuccessViewController() {
-
-    __weak IBOutlet UILabel *successMessage;
-    __weak IBOutlet UILabel *tradeItLabel;
     TTSDKUtils * utils;
+    __weak IBOutlet UIButton *tradeButton;
+    __weak IBOutlet UILabel *successMessage;
 }
 
 @end
 
 @implementation TTSDKSuccessViewController
+
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+
+    self.tradeSession = [TTSDKTicketSession globalSession];
+    utils = [TTSDKUtils sharedUtils];
+    
+    [successMessage setText:[NSString stringWithFormat:@"%@", [[self result] confirmationMessage]]];
+
+    [utils styleMainActiveButton:tradeButton];
+
+    NSMutableAttributedString * logoString = [[NSMutableAttributedString alloc] initWithAttributedString:[utils logoStringLight]];
+    [logoString addAttribute:NSFontAttributeName value:[UIFont boldSystemFontOfSize:17.0f] range:NSMakeRange(0, 7)];
+}
 
 -(void)willRotateToInterfaceOrientation:(UIInterfaceOrientation)toInterfaceOrientation duration:(NSTimeInterval)duration {
     [UIView setAnimationsEnabled:NO];
@@ -33,38 +47,17 @@
     [self.navigationItem setHidesBackButton:YES];
 }
 
-- (void)viewDidLoad {
-    [super viewDidLoad];
-
-    self.tradeSession = [TTSDKTicketSession globalSession];
-
-    utils = [TTSDKUtils sharedUtils];
-
-    [successMessage setText:[NSString stringWithFormat:@"%@", [[self result] confirmationMessage]]];
-    
-    NSMutableAttributedString * poweredBy = [[NSMutableAttributedString alloc]initWithString:@"powered by "];
-    NSMutableAttributedString * logoString = [[NSMutableAttributedString alloc] initWithAttributedString:[utils logoStringLight]];
-    [logoString addAttribute:NSFontAttributeName value:[UIFont boldSystemFontOfSize:17.0f] range:NSMakeRange(0, 7)];
-    [poweredBy appendAttributedString:logoString];
-    [tradeItLabel setAttributedText:poweredBy];
-}
-
-- (void)didReceiveMemoryWarning {
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
-}
 
 
 #pragma mark - Navigation
 
-// In a storyboard-based application, you will often want to do a little preparation before navigation
-- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
-    // Get the new view controller using [segue destinationViewController].
-    // Pass the selected object to the new view controller.
-}
-
 - (IBAction)closeButtonPressed:(id)sender {
     [TTSDKTradeItTicket returnToParentApp:self.tradeSession];
 }
+
+- (IBAction)tradeButtonPressed:(id)sender {
+    [self.navigationController popToRootViewControllerAnimated:YES];
+}
+
 
 @end
